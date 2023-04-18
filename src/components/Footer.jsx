858cffa751a2ca52aaef1bdd0a533010cc954b0f
NJ <109919004+JABnat@users.noreply.github.com>
2023-04-18 12:41:39 +0200

    build folder, clean code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import logo_footer from '../assets/images/logo_footer.svg'
 
 export default function Footer() {
     return (
-<div className="footer_containter">
-    <div className="text-container">
-<img className="logo-footer" src={logo_footer} alt="Kasa logo"/>
-<p className="ARR">© 2020 Kasa. All rights reserved</p>
-    </div>
-</div>
+        <div className="footer_containter">
+            <div className="text-container">
+                <img className="logo-footer" src={logo_footer} alt="Kasa logo"/>
+                <p className="ARR">© 2023 Kasa. All rights reserved</p>
+            </div>
+        </div>
     )
 }
